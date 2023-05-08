3be084d24c34b738d72668ab97f91eea64216ed4
William Hoppes <hoppesbeta@gmail.com>
2023-05-08 11:10:44 -0500

    First update test

--- R_Scratch.r
+++ R_Scratch.r
@@ -1,38 +1,32 @@
-
-
 decompose <- function (n) { 
-  possible_answers<-(1:(n-1))^2
-  first<-(rep(possible_answers, times=4))
-  second<-(rep(possible_answers, each=4))
-  df<-data.frame(first=first, second=second)
-  new_df<-data.frame(dummy=rep(0, times=nrow(df)*4))
-  for (i in 1:ncol(df)) {
-    new_df[, c(colnames(df)[i])]<-rep(df[, c(colnames(df)[i])], times=4)
-    new_df$third<-rep(possible_answers, each=4^2)
-  }
-  new_df<-subset(new_df, select=-c(dummy))
-  new_df<-new_df[, c("first", "second", "third")]
-  print(df)
-  print(new_df)
-}
-
-decompose <- function (n) { 
+  if (n<=2) {return(NULL)}
   target<-n^2
   possible_answers<-(1:(n-1))^2
-  df<-data.frame(a=possible_answers)
-  df<-merge(df, possible_answers, all=T)
-  colnames(df)<-c(letters[1:2])
-  df<-df[rowSums(df)<=target, ]
-  df<-merge(df, possible_answers, all=T)
-  colnames(df)<-c(letters[1:3])
-  df<-df[rowSums(df)<=target, ]
-  match_check=rowSums(df[,3]==df[, c(1,2)])
+  answers<-list()
+  df<-merge(data.frame(a=possible_answers), data.frame(b=possible_answers), all=T)
+  match_check<-df[,1]==df[, 2]
   match_check<-ifelse(match_check>0, FALSE, TRUE)
-  print(match_check)
   df<-df[match_check, ]
-  print(nrow(df))
-  return(df)
+  answer_check<-rowSums(df)==target
+  if (sum(answer_check)>0) {
+    answers<-append(answers, df[answer_check, ], (length(answers)+1))
+  }
+  for (itter in 3:n) {
+    df<-merge(df, possible_answers, all=T)
+    colnames(df)<-c(letters[1:itter])
+    df<-df[rowSums(df)<=target, ]
+    match_check<-rowSums(df[,itter]==df[, 1:itter-1])
+    match_check<-ifelse(match_check>0, FALSE, TRUE)
+    df<-df[match_check, ]
+    answer_check<-rowSums(df)==target
+    if (sum(answer_check)>0) {
+      answers<-append(answers, df[answer_check, ], (length(answers)+1))
+    }
+  }
+  return(answers)
 }
 
-print(decompose(5))
+print(Sys.time())
+print(decompose(11))
+print(Sys.time())
 
